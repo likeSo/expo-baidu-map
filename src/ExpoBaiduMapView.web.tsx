7ecf90a6d9dd8a7d84acae9b81396fcdd4287d43
Aron <Random1996@163.com>
2026-01-08 14:04:26 +0800

    fix: 临时去掉web支持

--- src/ExpoBaiduMapView.web.tsx
+++ src/ExpoBaiduMapView.web.tsx
@@ -5,11 +5,7 @@ import { ExpoBaiduMapViewProps } from './ExpoBaiduMap.types';
 export default function ExpoBaiduMapView(props: ExpoBaiduMapViewProps) {
   return (
     <div>
-      <iframe
-        style={{ flex: 1 }}
-        src={props.url}
-        onLoad={() => props.onLoad({ nativeEvent: { url: props.url } })}
-      />
+      
     </div>
   );
 }
